814e81134b2eacfacfa9c5b4ba70816286d417bc
Jozsef Hegedus <jhegedus@pm.me>
2019-12-24 23:50:12 +0100

    adding scalapy, fixing runtime error in progress
    Took 15 minutes

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,6 +2,10 @@ package example
 
 object Hello extends Greeting with App {
   println(greeting)
+  import me.shadaj.scalapy.py
+  val listLengthPython = py.global.len(List(1, 2, 3))
+  val listLength = listLengthPython.as[Int]
+  println(listLength)
 }
 
 trait Greeting {
